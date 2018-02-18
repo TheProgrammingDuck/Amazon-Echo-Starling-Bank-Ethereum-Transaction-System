1711688828eeeae3d766ba6cb11e2e9b7615eb7c
unknown <vsv501@york.ac.uk>
2018-02-18 09:13:34 +0000

    Alexa to relay interface.
    This includes a test alexa-app-server allowing you to test commands and
    their functionality locally. Including successful Ethereum transfers
    through Web3.
    
    It also includes the zip file that was uploaded to node.js.
    
    Eventually I'll also add the intents, custom slots and sample utterances
    we used.

---
+++ Alexa-Server/examples/apps/cash-to-eth/index.js
@@ -0,0 +1,88 @@
+var alexa = require('alexa-app');
+
+// Allow this module to be reloaded by hotswap when changed
+module.change_code = 1;
+
+// Define the alexa-app
+var app = new alexa.app('cash-to-eth');
+app.id = require('./package.json').alexa.applicationId;
+
+app.launch(function(req, res) {
+  //res.session('number', number);
+  //res.session('guesses', 0);
+  var prompt = "Hi, I'm the Starling Bank to Ethereum interface voice assistant. ";
+  res.say(prompt).reprompt(prompt).shouldEndSession(false);
+});
+
+app.intent('GetEth', {
+    "slots": { "AMOUNT": "NUMBER" },
+    "utterances": ["{Purchase| Buy | Give me | I want} {AMOUNT} {Ethereum | Eth | Ether | Cabbage} "]
+  },
+  function(req, res) {
+    var ethAmount = (+req.slot('AMOUNT'));
+
+    if (!ethAmount){
+      res.say("I'm not sure I heard how much ethereum you want to buy, could you please repeat that?");
+    } 
+    else if (ethAmount > 0) {
+      var cashToWithdraw = ethAmount * 681;
+      res.say("That's " + ethAmount + " ethereum");
+      res.say("The current exchange rate is 681 pounds to 1 ethereum");
+      res.say("I will now withdraw " + cashToWithdraw + " pounds.");
+
+    }
+
+    res.shouldEndSession(false);
+  }
+);
+
+app.intent('EtherInfo', {
+    "utterances": ["{How much ether do I have?|} "]
+  },
+  function(req, res) {
+    
+
+    res.say();
+    }
+
+    res.shouldEndSession(false);
+  }
+);
+
+app.intent('SendEther', {
+    "slots": { "AMOUNT": "NUMBER" , "ACCOUNT":"LITERAL"},
+    "utterances": ["{Send| Give} {AMOUNT} {Ethereum | Eth | Ether | Cabbage} {to |} {ACCOUNT} "]
+  },
+  function(req, res) {
+    var ethAmount = (+req.slot('AMOUNT'));
+    var account = (+req.slot('ACCOUNT'));
+
+    if (!ethAmount){
+      res.say("I'm not sure I heard how much ethereum you want to send, could you please repeat that?");
+    } 
+    else if (ethAmount > 0) {
+      if (!account){
+        res.say("I'm not sure I heard who you wanted to send that ether to, could you repeat that please?");
+      }
+      else {
+
+        //First check how much ether the person has. If he/she has enough then send requested amount to requested account.
+        //Else purchase the requested amount and send it.
+
+        //If cba then just purchase requested amount and send it.
+        var cashToWithdraw = ethAmount * 681;
+        res.say("That's " + ethAmount + " ethereum");
+        res.say("The current exchange rate is 681 pounds to 1 ethereum");
+        res.say("I will now withdraw " + cashToWithdraw + " pounds to purchase " + ethAmount + " ethereum. I will then send the ethereum to " + account + " .");
+
+      }
+
+    }
+
+    res.shouldEndSession(false);
+  }
+);
+
+
+
+module.exports = app;
---
+++ Alexa-Server/server.js
@@ -0,0 +1,6 @@
+var AlexaAppServer = require("../index.js");
+
+AlexaAppServer.start({
+  server_root: __dirname,
+  port: 8080
+  });
