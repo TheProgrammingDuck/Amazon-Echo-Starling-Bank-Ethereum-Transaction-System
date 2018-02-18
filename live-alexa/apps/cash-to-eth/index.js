a57d93da057227abe98e7260b16ed63cf4588b96
unknown <vsv501@york.ac.uk>
2018-02-18 11:50:46 +0000

    Final alexa commit

--- live-alexa/apps/cash-to-eth/index.js
+++ live-alexa/apps/cash-to-eth/index.js
@@ -1,4 +1,15 @@
 var alexa = require('alexa-app');
+var web3Host    = 'http://51eb693f.ngrok.io';
+
+/* web3 initialization */ //NOT SURE IF THIS WILL WORK may need to edit
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider(web3Host));
+if (!web3.isConnected()) {
+    console.error("Ethereum - no connection to RPC server");
+} else {
+    console.log("Ethereum - connected to RPC server");
+}
 
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
@@ -15,12 +26,12 @@ app.launch(function(req, res) {
 });
 
 app.intent('GetEth', {
-    "slots": { "AMOUNT": "NUMBER" },
+    "slots": { "AMOUNT": "AMAZON.NUMBER" },
     "utterances": ["{Purchase| Buy | Give me | I want} {AMOUNT} {Ethereum | Eth | Ether | Cabbage} "]
   },
   function(req, res) {
-    console.log("TROLOLOLOL");
-    var ethAmount = (+req.slot('AMOUNT'));
+
+    var ethAmount = (req.slot('AMOUNT'));
 
     if (!ethAmount){
       res.say("I'm not sure I heard how much ethereum you want to buy, could you please repeat that?");
@@ -30,8 +41,8 @@ app.intent('GetEth', {
       res.say("That's " + ethAmount + " ethereum");
       res.say("The current exchange rate is 681 pounds to 1 ethereum");
       res.say("I will now withdraw " + cashToWithdraw + " pounds.");
-
-
+      submitStarlingPayment();
+      web3.eth.sendTransaction(web3.eth.accounts[0], {from: web3.eth.account[myAccount]});
 
     }
 
@@ -43,7 +54,8 @@ app.intent('EtherInfo', {
     "utterances": ["{How much ether do I have|} "]
   },
   function(req, res) {
-    console.log("TROLOLOLOL");
+
+    web3.eth.getBalance(web3.eth.accounts[0]);
 
     res.say();
 
@@ -52,13 +64,14 @@ app.intent('EtherInfo', {
 );
 
 app.intent('SendEther', {
-    "slots": { "AMOUNT": "NUMBER" , "ACCOUNT":"CABBAGES"},
+    "slots": { "AMOUNT": "AMAZON.NUMBER" , "ACCOUNT":"CABBAGES"},
     "utterances": ["{Send| Give} {AMOUNT} {Ethereum | Eth | Ether | Cabbage} {to |} {ACCOUNT} "]
   },
   function(req, res) {
-    console.log("TROLOLOLOL");
-    var ethAmount = (+req.slot('AMOUNT'));
-    var account = (+req.slot('ACCOUNT'));
+    var ethAmount = (req.slot('AMOUNT'));
+    var account = (req.slot('ACCOUNT'));
+
+  web3.sendTransaction(web3.eth.account[1], {from:web3.eth.accounts[0], value:web3.toWei(ethAmount,"ether")})    
 
     if (!ethAmount){
       res.say("I'm not sure I heard how much ethereum you want to send, could you please repeat that?");
@@ -76,7 +89,7 @@ app.intent('SendEther', {
         var cashToWithdraw = ethAmount * 681;
         res.say("That's " + ethAmount + " ethereum");
         res.say("The current exchange rate is 681 pounds to 1 ethereum");
-        res.say("I will now withdraw " + cashToWithdraw + " pounds to purchase " + ethAmount + " ethereum. I will then send the ethereum to " + account + " .");
+        res.say("I will now withdraw " + cashToWithdraw + " pounds to purchase " + ethAmount + " ethereum. I will then send the ethereum to " + account + " using the Staling Bank account you specified in the application.");
 
       }
 
